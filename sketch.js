e68e4572c1bef06fd4f1b3b820e463567fbf5a5c
sagarpitalwar <sagar.balienterprises@gmail.com>
2021-05-08 16:56:51 +0530

    created constrait

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -34,7 +35,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 = new Log(200,200,80,PI/2);
+
     bird = new Bird(100,100);
+    chain= new Chain(bird.body,log6.body)
+   
 
 }
 
@@ -58,7 +63,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
+    chain.display();
 
     bird.display();
     platform.display();
+    
 }
